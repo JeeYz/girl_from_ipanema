6305da5581d47881e62cc84016bc2b7a3ed0e01d
JeeYz <jkdspiegel@gmail.com>
2020-07-10 11:55:23 +0900

    backup

--- preprocessing_for_data.py
+++ preprocessing_for_data.py
@@ -4,16 +4,16 @@
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MaxAbsScaler
+from sklearn.preprocessing import Normalizer
+from sklearn import preprocessing
 
 #%% standardization
-def make_standar(input_numpy):
-    
+def make_standar(input_numpy):    
     scaler = StandardScaler()
     scaler.fit(input_numpy)
     result = scaler.transform(input_numpy)
     
     return result
-
 
 
 #%% normalization -> max abs scale
@@ -24,10 +24,50 @@ def make_MaxAbsScaler(input_numpy):
     return result
 
 
-#%% reshape for cnn
-def reshape_for_cnn(input_numpy):
-    
+#%% standard normalization
+def make_normalization(input_numpy):
+    normalizer = Normalizer().fit(input_numpy)
+    result = normalizer.transform(input_numpy)
     
     return result
 
+
+#%% make normalization by np.linalg
+def make_norm_nplinalg(input_numpy):
+    result = input_numpy/np.linalg.norm(input_numpy)
+    
+    return result
+    
+
+#%% make normalization with numpy
+def make_norm_numpy(input_numpy):
+    input_min, input_max = input_numpy.min(), input_numpy.max()
+    result = (input_numpy - input_min)/(input_max - input_min)
+    
+    return result
+
+
+#%%
+def make_stand_numpy(input_numpy):
+    result = (input_numpy - np.mean(input_numpy))/np.std(input_numpy)
+    
+    return result
+
+
+#%% reshape for cnn #0
+def reshape_for_1dcnn(input_numpy, rate):
+    result = np.reshape(input_numpy, (len(input_numpy), rate*2, 1))
+    
+    return result
+
+#%% reshape for cnn #1
+def reshape_for_2dcnn(input_numpy):
+    result = np.reshape(input_numpy, \
+                    (len(input_numpy), len(input_numpy[0]), \
+                     len(input_numpy[0][0]), 1))
+    
+    return result
+
+
+#%%
 
---
+++ rnn_class.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+#%%
+class cnn_rnn_for_asr(layers.Layer):
+    def __init__():
+        
+        pass
+
+
+
