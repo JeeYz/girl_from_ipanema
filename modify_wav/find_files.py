61db6d0abb43a35447c49ab707ab04655fdeb8be
JeeYz <jkdspiegel@gmail.com>
2021-04-09 16:02:39 +0900

    delete all files move to backup repository

--- modify_wav/find_files.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-
-def find_files(filepath, file_ext):
-    for (path, dir, files) in os.walk(filepath):
-        for filename in files:
-            ext = os.path.splitext(filename)[-1]
-            if ext == file_ext:
-                print("%s\\%s" % (path, filename))
