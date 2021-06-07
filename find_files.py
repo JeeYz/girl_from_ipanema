649d9f3e7e567fe31ce5f5ebe72e555ae7b07269
Jeey <jkdspiegel@gmail.com>
2021-06-07 20:22:07 +0900

    backup

--- find_files.py
+++ find_files.py
@@ -8,22 +8,29 @@ method0 : find files
 class utility_functions():
 
     def __init__(self):
-        self.files_list = list()
         return
 
 
     def find_files(self, **kwargs):
 
+        files_list = list()
+
         if 'target_path' in kwargs.keys():
             target_path = kwargs['target_path']
-        if 'target_ext' in kwargs.keys():
-            target_ext = kwargs['target_ext']
+        if 'target_file_format' in kwargs.keys():
+            target_file_format = kwargs['target_file_format']
 
         for (path, dir, files) in os.walk(target_path):
             for filename in files:
                 ext = os.path.splitext(filename)[-1]
-                if ext == target_ext:
+                if ext == target_file_format:
                     print("%s/%s" % (path, filename))
                     self.files_list.append(path+'\\'+filename)
     
-        return
+        return files_list
+
+
+
+
+
+
