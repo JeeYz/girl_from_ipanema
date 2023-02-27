b2db8b8adeb9924f867f8308a6c298afb4a3260c
JeeYz <jy@MacBookAir.local>
2023-02-27 20:54:34 +0900

    game module folder

---
+++ game_module/aa.py
@@ -0,0 +1,27 @@
+import pygame
+import numpy as np
+
+
+#%% function
+def print_log():
+    return
+
+
+#%% class
+'''
+class 
+'''
+class classname(object):
+
+    pass
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    pass
+
